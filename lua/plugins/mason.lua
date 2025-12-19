fcab191eec4fdf8729772f01f07ef6eb36f9bcac
Federico <16429579+rawnly@users.noreply.github.com>
2025-12-19 21:01:32 +0100

    configure mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,19 +6,52 @@ return {
         "lua_ls",
         "gopls",
         "zls",
-        "biome"
+        "biome",
+        "jsonls",
+        "yamlls",
+        "docker_compose_language_service",
+        "ruff",
+        "pyright"
       },
       handlers = {
         function(server_name)
           if server_name == "tsserver" or server_name == "ts_ls" then
             return
           end
+          require("lspconfig")[server_name].setup({})
+        end,
+        jsonls = function()
+          require('lspconfig').jsonls.setup {
+            settings = {
+              json = {
+                schemas = require('schemastore').json.schemas {
+                  extra = {
+                    {
+                      description = "ecs task definition",
+                      fileMatch = "task-*.json",
+                      name = "ecs-task-definition",
+                      url = "https://ecs-intellisense.s3-us-west-2.amazonaws.com/task-definition/schema.json"
+                    }
+                  }
+                },
+                validate = { enable = true },
+              },
+              yaml = {
+                schemaStore = {
+                  enable = true,
+                  url = ""
+                },
+                schemas = require('schemastore').yaml.schemas()
+              },
+            },
+          }
         end
       }
     },
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
       "neovim/nvim-lspconfig",
+      "b0o/schemastore.nvim",
     },
   }
 }
