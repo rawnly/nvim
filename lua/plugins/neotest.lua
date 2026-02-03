7343dfe65459e5a36109143e0a85060e9f40e4a3
Federico <16429579+rawnly@users.noreply.github.com>
2026-02-03 10:51:20 +0100

    vitest neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,19 +1,83 @@
 return {
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"mrcjkb/rustaceanvim",
-		},
-		config = function()
-			require("neotest").setup({
-				adapters = {
-					require("rustaceanvim.neotest"),
-				},
-			})
-		end,
-	},
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "mrcjkb/rustaceanvim",
+      "marilari88/neotest-vitest"
+    },
+    config = function()
+      require("neotest").setup {
+        adapters = {
+          require("neotest-vitest"),
+          require("rustaceanvim.neotest")
+        },
+        summary = {
+          open = "botright vsplit | vertical resize 80",
+          follow = true,
+        }
+      }
+    end,
+    keys = {
+      {
+        "<leader>n",
+        group = "🧪 Test",
+        nowait = true,
+        remap = false,
+      },
+      {
+        "<leader>nr",
+        "<cmd>lua require('neotest').run.run()<cr>",
+        desc = "Run nearest test",
+      },
+      {
+        "<leader>nf",
+        "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+        desc = "Run current file",
+      },
+      {
+        "<leader>na",
+        "<cmd>lua require('neotest').run.run({ suite = true })<cr>",
+        desc = "Run all tests",
+      },
+      {
+        "<leader>nd",
+        "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+        desc = "Debug nearest test",
+      },
+      {
+        "<leader>ns",
+        "<cmd>lua require('neotest').run.stop()<cr>",
+        desc = "Stop test",
+      },
+      {
+        "<leader>nn",
+        "<cmd>lua require('neotest').run.attach()<cr>",
+        desc = "Attach to nearest test",
+      },
+      {
+        "<leader>no",
+        "<cmd>lua require('neotest').output.open()<cr>",
+        desc = "Show test output",
+      },
+      {
+        "<leader>np",
+        "<cmd>lua require('neotest').output_panel.toggle()<cr>",
+        desc = "Toggle output panel",
+      },
+      {
+        "<leader>nv",
+        "<cmd>lua require('neotest').summary.toggle()<cr>",
+        desc = "Toggle summary",
+      },
+      {
+        "<leader>nc",
+        "<cmd>lua require('neotest').run.run({ suite = true, env = { CI = true } })<cr>",
+        desc = "Run all tests with CI",
+      },
+    }
+  },
 }
