fb0e32140f43bc75b482874c5a98fac58def15e5
Federico <16429579+rawnly@users.noreply.github.com>
2026-02-04 01:53:53 +0100

    improved treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,39 +1,55 @@
 ---@module "lazy.nvim"
 ---@type LazySpec
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy   = false,
-    build  = ':TSUpdate',
-    opts   = {
-      sync_install = true,
-      auto_install = true,
-      highlight = { enable = true },
-      ensure_installed = {
-        "lua",
-        "vim",
-        "rust",
-        "go",
-        "astro",
-        "bash",
-        "fish",
-        "zig",
-        "tsx",
-        "caddy",
-        "nginx",
-        "yaml",
-        "toml",
-        "css",
-        "html",
-        "markdown",
-        "markdown_inline",
-        "typescript",
-        "javascript",
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.install").prefer_git = true
-      require("nvim-treesitter.configs").setup(opts)
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		opts = {
+			sync_install = true,
+			auto_install = true,
+			highlight = { enable = true },
+			ensure_installed = {
+				"lua",
+				"vim",
+				"rust",
+				"go",
+				"astro",
+				"bash",
+				"fish",
+				"zig",
+				"tsx",
+				"caddy",
+				"nginx",
+				"yaml",
+				"toml",
+				"css",
+				"html",
+				"markdown",
+				"markdown_inline",
+				"typescript",
+				"javascript",
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.install").prefer_git = true
+			require("nvim-treesitter.configs").setup(opts)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local buf = args.buf
+					local ft = args.match
+
+					local lang = vim.treesitter.language.get_lang(ft) or ft
+
+					if not vim.treesitter.language.add(lang) then
+						return
+					end
+
+					vim.treesitter.start(buf, lang)
+					-- vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
+	},
 }
