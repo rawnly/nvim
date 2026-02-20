6323612b09ea878c49f1b9597ab49ab3c4b0b076
Federico <16429579+rawnly@users.noreply.github.com>
2026-02-20 09:15:17 +0100

    better defaults

--- init.lua
+++ init.lua
@@ -1,28 +1,40 @@
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-local mappings = require("mappings")
-
 local options = {
-	tabstop = 2,
-	smartindent = true,
-	shiftwidth = 2,
-	expandtab = true,
-	number = true,
-	termguicolors = true,
-	clipboard = "unnamedplus",
-	foldmethod = "expr",
-	foldexpr = "v:lua.vim.treesitter.foldexpr()",
-	foldcolumn = "0",
-	foldtext = "",
-	foldlevel = 99,
-	foldlevelstart = 99,
-	foldnestmax = 4,
-	guifont = "Berkeley Mono Variable",
+  tabstop = 2,
+  smartindent = true,
+  shiftwidth = 2,
+  expandtab = true,
+  number = true,
+  termguicolors = true,
+  clipboard = "unnamedplus",
+  foldmethod = "expr",
+  foldexpr = "v:lua.vim.treesitter.foldexpr()",
+  foldcolumn = "0",
+  foldtext = "",
+  foldlevel = 99,
+  foldlevelstart = 99,
+  foldnestmax = 4,
+  guifont = "Berkeley Mono Variable",
+  autoindent = true,
+  -- search
+  hlsearch = false,
+  ignorecase = true,
+  smartcase = true,
+
+  -- files
+  undofile = true,
+  autoread = true,
+  undodir = vim.fn.expand("~/.vim/undodir"),
+
+  -- split
+  splitbelow = true,
+  splitright = true
 }
 
 for option, value in pairs(options) do
-	vim.opt[option] = value
+  vim.opt[option] = value
 end
 
 vim.g.mapleader = " "
@@ -34,6 +46,6 @@ require("polish")
 vim.cmd.colorscheme("nyx")
 
 if vim.lsp.inlay_hint then
-	-- Enable inlay hints by default
-	vim.lsp.inlay_hint.enable()
+  -- Enable inlay hints by default
+  vim.lsp.inlay_hint.enable()
 end
