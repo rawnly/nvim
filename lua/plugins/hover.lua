c2cbdb71680af079aab22035af652f9ad55c93a9
Federico <16429579+rawnly@users.noreply.github.com>
2026-02-23 15:11:19 +0100

    add hover.nvim

---
+++ lua/plugins/hover.lua
@@ -0,0 +1,28 @@
+return {
+  "lewis6991/hover.nvim",
+  opts = {
+    providers = {
+      'hover.providers.diagnostic',
+      'hover.providers.lsp',
+      'hover.providers.dap',
+      'hover.providers.man',
+      'hover.providers.dictionary',
+      'hover.providers.gh',
+      'targetprocess.plugins.hover'
+    }
+  },
+  keys = {
+    {
+      'K',
+      function() require('hover').open() end,
+      mode = 'n',
+      desc = "hover.nvim (open)",
+    },
+    {
+      'gK',
+      function() require('hover').enter() end,
+      mode = 'n',
+      desc = "hover.nvim (enter)",
+    }
+  }
+}
