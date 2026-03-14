dc19008e2414bb2732630d9e0366b22cb933a2e6
Federico <16429579+rawnly@users.noreply.github.com>
2026-03-14 22:09:36 +0100

    new themes

--- init.lua
+++ init.lua
@@ -1,57 +1,59 @@
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
+local colorscheme = "oc-2"
+
 local options = {
-  tabstop = 2,
-  smartindent = true,
-  shiftwidth = 2,
-  expandtab = true,
-  number = true,
-  termguicolors = true,
-  clipboard = "unnamedplus",
-  foldmethod = "expr",
-  foldexpr = "v:lua.vim.treesitter.foldexpr()",
-  foldcolumn = "0",
-  foldtext = "",
-  foldlevel = 99,
-  foldlevelstart = 99,
-  foldnestmax = 4,
-  guifont = "Berkeley Mono Variable",
-  autoindent = true,
-  -- search
-  hlsearch = false,
-  ignorecase = true,
-  smartcase = true,
+	tabstop = 2,
+	smartindent = true,
+	shiftwidth = 2,
+	expandtab = true,
+	number = true,
+	termguicolors = true,
+	clipboard = "unnamedplus",
+	foldmethod = "expr",
+	foldexpr = "v:lua.vim.treesitter.foldexpr()",
+	foldcolumn = "0",
+	foldtext = "",
+	foldlevel = 99,
+	foldlevelstart = 99,
+	foldnestmax = 4,
+	guifont = "Berkeley Mono Variable",
+	autoindent = true,
+	-- search
+	hlsearch = false,
+	ignorecase = true,
+	smartcase = true,
 
-  -- files
-  undofile = true,
-  autoread = true,
-  undodir = vim.fn.expand("~/.vim/undodir"),
+	-- files
+	undofile = true,
+	autoread = true,
+	undodir = vim.fn.expand("~/.vim/undodir"),
 
-  -- split
-  splitbelow = true,
-  splitright = true
+	-- split
+	splitbelow = true,
+	splitright = true,
 }
 
 for option, value in pairs(options) do
-  vim.opt[option] = value
+	vim.opt[option] = value
 end
 
 local globals = {
-  mapleader = " ",
-  maplocalleader = "\\"
+	mapleader = " ",
+	maplocalleader = "\\",
 }
 
 for opt, value in pairs(globals) do
-  vim.g[opt] = value
+	vim.g[opt] = value
 end
 
 require("config.lazy")
 require("polish")
 
-vim.cmd.colorscheme("koda")
+vim.cmd.colorscheme(colorscheme)
 
 if vim.lsp.inlay_hint then
-  -- Enable inlay hints by default
-  vim.lsp.inlay_hint.enable()
+	-- Enable inlay hints by default
+	vim.lsp.inlay_hint.enable()
 end
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,34 +1,36 @@
 ---@type LazySpec
 return {
-  {
-    "oskarnurm/koda.nvim",
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {},
-  },
-  -- Lua
-  {
-    "f-person/auto-dark-mode.nvim",
-    enabled = false,
-    opts = {
-      update_interval = 500,
-    },
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    priority = 1000,
-    enabled = false,
-    opts = {
-      style = "night",
-      transparent = false,
-      styles = {
-        keywords = {
-          italic = true,
-          bold = true,
-        },
-      },
-      dim_inactive = true,
-    }, -- Or 'day'/'moon'
-  },
+	{ "shaunsingh/moonlight.nvim" },
+	{ "0xleodevv/oc-2.nvim" },
+	{
+		"oskarnurm/koda.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		opts = {},
+	},
+	-- Lua
+	{
+		"f-person/auto-dark-mode.nvim",
+		enabled = false,
+		opts = {
+			update_interval = 500,
+		},
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		priority = 1000,
+		enabled = false,
+		opts = {
+			style = "night",
+			transparent = false,
+			styles = {
+				keywords = {
+					italic = true,
+					bold = true,
+				},
+			},
+			dim_inactive = true,
+		}, -- Or 'day'/'moon'
+	},
 }
